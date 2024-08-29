1e6765257d4923fd2b14aa0d331500ff6d906baf
dancpluz <dan08jan@gmail.com>
2024-08-29 19:04:49 -0300

    feat: add homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,72 @@
-export default function Home() {
+'use client'
+
+import About from "@/components/About";
+import Header from "@/components/Header";
+import Projects from "@/components/Projects";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useRef } from "react";
+
+const queryClient = new QueryClient()
+
+export default function HomePage() {
+  const sections = {
+    inicio: {
+      ref: useRef<HTMLDivElement | null>(null),
+      id: 'inicio',
+    },
+    projetos: {
+      ref: useRef<HTMLDivElement | null>(null),
+      id: 'projetos',
+      icon: 'gem.svg',
+    },
+    sobre: {
+      ref: useRef<HTMLDivElement | null>(null),
+      id: 'sobre',
+      icon: 'user.svg',
+    },
+    habilidades: {
+      ref: useRef<HTMLDivElement | null>(null),
+      id: 'habilidades',
+      icon: 'lamp.svg',
+    },
+    contato: {
+      ref: useRef<HTMLDivElement | null>(null),
+      id: 'contato',
+      icon: 'phone.svg',
+    },
+    final: {
+      ref: useRef<HTMLDivElement | null>(null),
+      id: 'final',
+      icon: 'phone.svg',
+    }
+  }
+
   return (
-    <main className='bg-background'>
-      <div className='w-6 h-6  bg-foreground'/>
-    </main>
-  );
+    <QueryClientProvider client={queryClient}>
+      <Header sections={sections} />
+      <div id={sections.inicio.id} ref={sections.inicio.ref} className='h-[100vh]'>
+        <h1>Home</h1>
+      </div>
+      <div id={sections.projetos.id} ref={sections.projetos.ref} className=''>
+        <Projects />
+      </div>
+      <div id={sections.sobre.id} ref={sections.sobre.ref} className='h-[100vh]'>
+        <About />
+      </div>
+      <div id={sections.habilidades.id} ref={sections.habilidades.ref} className='h-[100vh]'>
+        <h1>Habilidades</h1>
+      </div>
+      <div id={sections.contato.id} ref={sections.contato.ref} className='h-[100vh]'>
+        <h1>Contato</h1>
+      </div>
+      <div id={sections.final.id} ref={sections.final.ref} className='h-[30vh]'>
+        <h1>Rodape</h1>
+      </div>
+      {/*
+      <div ref={fiveRef} className='h-[30vh]'>
+        <h1>Footer</h1>
+      </div>
+      */}
+    </QueryClientProvider>
+  )
 }
