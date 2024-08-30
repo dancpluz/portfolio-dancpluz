89a791fa3110aa846c4448a0cdbb06bd88f99508
dancpluz <dan08jan@gmail.com>
2024-08-29 22:07:04 -0300

    feat: improve api and utils

--- lib/api.ts
+++ lib/api.ts
@@ -2,21 +2,32 @@ import PocketBase from 'pocketbase';
 
 const pb = new PocketBase('http://hub.ruadebaixo.com.br:1002');
 
-export function buildUrl(record, firstFilename) {
+export function buildImageUrl(record, firstFilename: string) {
   return pb.files.getUrl(record, firstFilename)
 }
 
 export async function getProjects() {
   try {
     const records = await pb.collection('projects').getFullList({
-      sort: '-created',
       expand: 'icons',
     });
-    console.log('Resultado:',records)
+    return records;
+  }
+  catch (error) {
+    throw(error);
+  }
+}
+
+export async function getTechnologies() {
+  try {
+    const records = await pb.collection('icons').getFullList({
+      filter: 'technology = true',
+    });
+    console.log('Resultado:', records)
     return records;
   }
   catch (error) {
     console.log('Erro: ', error)
-    throw(error);
+    throw (error);
   }
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,39 @@
 export const angleToRadians = (angle: number) => {
   return angle * (Math.PI / 180)
 }
+
+export function formatDate(dateString: string) {
+  const date = new Date(dateString);
+
+  const months = [
+    'JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN',
+    'JUL', 'AGO', 'SET', 'OUT', 'NOV', 'DEZ'
+  ];
+
+  const month = months[date.getUTCMonth()];
+  const year = date.getUTCFullYear().toString().slice(-2);
+
+  return `${month} ${year}`;
+}
+
+export function formatTimeDifference(dateString) {
+  const inputDate = new Date(dateString);
+  const today = new Date();
+
+  const diffMs = today - inputDate;
+
+  const years = Math.floor(diffMs / (1000 * 60 * 60 * 24 * 365.25));
+  const months = Math.floor((diffMs % (1000 * 60 * 60 * 24 * 365.25)) / (1000 * 60 * 60 * 24 * 30.44));
+
+  if (years > 0) {
+    if (months >= 6) {
+      return years === 1 ? "1 ANO E MEIO" : `${years} ANOS E MEIO`;
+    } else {
+      return years === 1 ? "1 ANO" : `${years} ANOS`;
+    }
+  } else if (months > 0) {
+    return months === 1 ? "1 MÊS" : `${months} MESES`;
+  } else {
+    return "MENOS DE 1 MÊS";
+  }
+}
