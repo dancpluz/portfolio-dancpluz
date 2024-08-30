4e9b1ad0b5ac70421f89395165526bf932018b3a
dancpluz <dan08jan@gmail.com>
2024-08-29 22:08:12 -0300

    feat: fetch icons from db

--- components/About.tsx
+++ components/About.tsx
@@ -1,19 +1,21 @@
+import { buildImageUrl, getTechnologies } from '@/lib/api'
+import { useQuery } from '@tanstack/react-query'
 import Image from 'next/image'
 
 function Technologies({ technologies }) {
   return (
     <div className='flex overflow-hidden justify-between gap-8 [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]'>
-      <ul className='min-w-full flex items-center shrink-0 gap-8 justify-between animate-infinite-scroll-h'>
-        {technologies.map(({ src, alt }) => (
-          <li className='w-16' key={alt}>
-            <Image  src={src} width={64} height={64} alt={alt} />
+      <ul aria-hidden='true' className='min-w-full flex items-center shrink-0 gap-8 justify-between animate-infinite-scroll-h'>
+        {technologies.map((tech) => (
+          <li className='w-16' key={tech.alt}>
+            <Image src={buildImageUrl(tech, tech.icon)} width={64} height={64} alt={tech.alt} />
           </li>
         ))}
       </ul>
       <ul aria-hidden='true' className='min-w-full flex items-center shrink-0 gap-8 justify-between animate-infinite-scroll-h'>
-        {technologies.map(({ src, alt }) => (
-          <li className='w-16' key={alt}>
-            <Image src={src} width={64} height={64} alt={alt} />
+        {technologies.map((tech) => (
+          <li className='w-16' key={tech.alt}>
+            <Image src={buildImageUrl(tech, tech.icon)} width={64} height={64} alt={tech.alt} />
           </li>
         ))}
       </ul>
@@ -22,9 +24,22 @@ function Technologies({ technologies }) {
 }
 
 export default function About() {
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ['technologies'],
+    queryFn: getTechnologies,
+  })
+
+  if (isPending) {
+    return <span>Loading...</span>
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>
+  }
+  
   return (
     <div>
-      <Technologies technologies={[{ src: 'camera.svg', alt: 'teste' }, { src: 'insta.svg', alt: 'Insta' }, { src: 'gem.svg', alt: 'henn' }, { src: 'lamp.svg', alt: 'teste' }, { src: 'logo.svg', alt: 'teste' }, { src: 'phone.svg', alt: 'teste' }, { src: 'user.svg', alt: 'teste' }, { src: 'link.svg', alt: 'teste' }, { src: 'lamp.svg', alt: 'teste' }, { src: 'logo.svg', alt: 'teste' }, { src: 'phone.svg', alt: 'teste' }, { src: 'user.svg', alt: 'teste' }, { src: 'camera.svg', alt: 'teste' }, { src: 'insta.svg', alt: 'Insta' }, { src: 'gem.svg', alt: 'henn' }, { src: 'lamp.svg', alt: 'teste' }, { src: 'logo.svg', alt: 'teste' }, { src: 'phone.svg', alt: 'teste' }, { src: 'user.svg', alt: 'teste' }, { src: 'link.svg', alt: 'teste' }, { src: 'lamp.svg', alt: 'teste' }, { src: 'logo.svg', alt: 'teste' }, { src: 'phone.svg', alt: 'teste' }, { src: 'user.svg', alt: 'teste' }]} />
+      <Technologies technologies={data} />
     </div>
   )
 }
