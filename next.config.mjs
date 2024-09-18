7ecf2cc89f6b41527abd8b494642bab39cd3a2e5
dancpluz <dan08jan@gmail.com>
2024-09-18 20:42:32 -0300

    fix: cors shit

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,26 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
+        protocol: 'https',
         hostname: 'hub.ruadebaixo.com.br',
         port: '1002',
         pathname: '/api/files/**',
       },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/api/files/**',
+      },
     ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://hub.ruadebaixo.com.br:1002/api/:path*',
+      },
+    ]
   },
 };
 
