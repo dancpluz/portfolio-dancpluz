01d27f5d5b233e7bac321cfd1035a5a13654d281
dancpluz <dan08jan@gmail.com>
2024-09-25 16:50:14 -0300

    feat: add footer animation

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,93 +1,104 @@
-import { motion } from "framer-motion";
+import { motion, useCycle, AnimatePresence } from "framer-motion";
+import { useEffect } from "react";
+import useMeasure from "react-use-measure";
 
 export default function Footer() {
   return (
     <>
-      <div className='px-40 justify-end mr-[400px] py-32 flex gap-16 grow'>
-        <div className='flex flex-col'>
-          <p className='text-2xl font-extrabold'>VAMOS</p>
-          <p className='text-2xl font-extrabold'>FAZER</p>
-          <p className='text-2xl font-extrabold'>UM</p>
+        <div className='px-40 items-center ml-[100px] py-32 flex gap-16'>
+          <div className='flex flex-col'>
+            <p className='text-2xl font-extrabold'>VAMOS</p>
+            <p className='text-2xl font-extrabold'>FAZER</p>
+            <p className='text-2xl font-extrabold'>UM</p>
+          </div>
+          <RevealLinks />
+          {/* <h1 className='text-center text-8xl font-extrabold'>PROJETO UI/UX</h1> */}
+          {/* <h1 layout className='text-center text-8xl font-extrabold'>?</h1> */}
         </div>
-        <RevealLinks />
-        {/* <h1 className='text-center text-8xl font-extrabold'>PROJETO UI/UX</h1> */}
-        <h1 className='text-center text-8xl font-extrabold'>?</h1>
-      </div>
-      <div className='w-full bg-foreground py-1'>
-        <p className='text-center font-regular text-background'>TODOS OS DIRETOS RESERVADOS DANIEL LUZ </p>
-      </div>
+        <div className='w-full bg-foreground py-1'>
+          <p className='text-center font-regular text-background'>TODOS OS DIRETOS RESERVADOS DANIEL LUZ </p>
+        </div>
     </>
   )
 }
 
 export const RevealLinks = () => {
   return (
-    <section className="grid place-content-center gap-2 text-foreground">
-      <FlipLink href="#">Twitter</FlipLink>
-    </section>
+    <motion.section layout className="gridplace-content-left gap-2 text-foreground">
+      <FlipLinks />
+    </motion.section>
   );
 };
 
 const DURATION = 0.25;
 const STAGGER = 0.025;
+const INTERVAL = 4;
 
-const FlipLink = ({ children, href }: { children: string; href: string }) => {
+export function FlipLinks() {
+  const texts = ["Site", "vídeo", "curso", "Portfolio", "Ecommerce", "aplicativo"];
+
+  const [animationState, cycleAnimationState] = useCycle(...texts.map((_, i) => i.toString()));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      cycleAnimationState();
+    }, INTERVAL * 1000);
+
+    // Clear interval when component unmounts
+    return () => clearInterval(interval);
+  }, [cycleAnimationState]);
+
   return (
-    <motion.a
-      initial="initial"
-      whileHover="hovered"
-      href={href}
-      className="relative w-full base overflow-hidden whitespace-nowrap text-8xl font-black uppercase"
+    <motion.div
+      animate={animationState}
+      className="relative base overflow-hidden whitespace-nowrap text-8xl font-black uppercase"
       style={{
-        lineHeight: 0.75,
+        lineHeight: 1,
       }}
     >
+      <AnimatePresence mode="wait">
+        <motion.div className='flex' layout>
+          <motion.h1 className='w-auto text-transparent tracking-widest' layout>
+            {texts[Number(animationState)]}
+          </motion.h1>
+          <motion.h1 className='w-auto text-foreground tracking-widest' layout>
+            ?
+          </motion.h1>
+        </motion.div>
+      </AnimatePresence>
       <div>
-        {children.split("").map((l, i) => (
-          <motion.span
-            variants={{
-              initial: {
-                y: 0,
-              },
-              hovered: {
-                y: "-100%",
-              },
-            }}
-            transition={{
-              duration: DURATION,
-              ease: "easeInOut",
-              delay: STAGGER * i,
-            }}
-            className="inline-block"
-            key={i}
-          >
-            {l}
-          </motion.span>
-        ))}
+        {texts.map((text,index) => {
+          const nextIndex = (index + 1) % texts.length
+          const prevIndex = (index - 1 + texts.length) % texts.length;
+
+          const variant = texts.reduce((acc, _, i) => {
+            if (i === index) return { ...acc, [i]: { y: 0, opacity: 1 } }
+            else if (i === prevIndex) return { ...acc, [i]: { y: "100%", opacity: 0 } }
+            else if (i === nextIndex) return { ...acc, [i]: { y: "-100%", opacity: 0 } }
+            else return { ...acc, [i]: { y: "-100%", opacity: 0 } }
+          }, {});
+          console.log(variant)
+        return (
+          <div key={text} className='absolute inset-0'>
+            {text.split("").map((l, i) => (
+              <motion.span
+                key={i}
+                variants={variant}
+                transition={{
+                  duration: DURATION,
+                  ease: "easeInOut",
+                  delay: STAGGER * i,
+                }}
+                className="inline-block"
+              >
+                {l}
+              </motion.span>
+              )   
+            )}
+          </div>
+        )}
+      )}
       </div>
-      <div className="absolute inset-0">
-        {'children'.split("").map((l, i) => (
-          <motion.span
-            variants={{
-              initial: {
-                y: "100%",
-              },
-              hovered: {
-                y: 0,
-              },
-            }}
-            transition={{
-              duration: DURATION,
-              ease: "easeInOut",
-              delay: STAGGER * i,
-            }}
-            className="inline-block"
-            key={i}
-          >
-            {l}
-          </motion.span>
-        ))}
-      </div>
-    </motion.a>
+    </motion.div>
   );
 };
